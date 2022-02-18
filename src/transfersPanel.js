8a83103c08f28cd89127852f0401dbd64146b878
Deniz Kukul <deniz_kukul@hotmail.com>
2022-02-18 23:55:40 +0300

    Revize-3

--- src/transfersPanel.js
+++ src/transfersPanel.js
@@ -1,94 +1,14 @@
-// Main component renders body element
-class App extends Component {
-  constructor(args) {
-    const users = new State([]);
-    const history = new State([]);
-    // Calling parent constructor this way calls initial render after childComponents and states are set
-    // This only creates the states, if this component needs to subscribe, they should also be added to argummets as subscriptions array
-    // super({...args, users, history, subscriptions:[...args.subscriptions,users,history]})
-    super({ ...args, users, history });
-  }
+// Transfers-Panel
+class TransfersPanel extends Component {
   template() {
     return (`
-    <div id="${this.targetElementID}" class="app">
-      <div class="users-panel">
-        ${new NewUser({ users: this.users, history: this.history, parentComponent: this }).target()}
-        ${new UserList({ users: this.users, history: this.history, subscriptions: [this.users], parentComponent: this }).target()}
-      </div>
-      <div class="transfers-panel">
-        ${new NewTransfer({ users: this.users, history: this.history, subscriptions: [this.users], parentComponent: this }).target()}
-        ${new HistoryLogs({ users: this.users, history: this.history, parentComponent: this }).target()}
-      </div>
-    </div>
+      ${new NewTransfer({ users: this.users, history: this.history, subscriptions: [this.users], parentComponent: this }).target()}
+      ${new HistoryLogs({ users: this.users, history: this.history, parentComponent: this }).target()}
     `)
   }
 }
 
-// Component adds new users to the list
-class NewUser extends Component {
-  addUser(e) {
-    e.preventDefault();
-    // Get input elements
-    const usernameInput = this.targetElement.querySelector(".username-input");
-    const balanceInput = this.targetElement.querySelector(".balance-input");
-    // Add new user to users state
-    let uid = this.generateID()
-    this.users.setValue([...this.users.value, { name: usernameInput.value, uid: uid, balance: Number(balanceInput.value) }])
-    this.history.setValue([...this.history.value, { id: this.generateID(), type: "adduser", time: this.getTime(), username: usernameInput.value, uid: uid, balance: balanceInput.value }])
-    // Clear input fields
-    usernameInput.value = "";
-    balanceInput.value = "";
-  }
-  template() {
-    return (`
-      <div class="title"> Add new user </div>
-      <form class="adduser-form">
-        <div class="inputs-container">
-          <input class="username-input" placeholder="Name" required pattern="([a-zA-Z]+[a-zA-Z ]+)" maxlength="20"/>
-          <input class="balance-input" type="number" placeholder="Balance" required min="0" max="999999999"/>
-        </div>
-        <div class="button-container">
-          <button class="adduser-button"> Add </button>
-        </div>
-      </form>
-    `);
-  }
-  addListeners() {
-    this.targetElement.querySelector(".adduser-form").addEventListener("submit", this.addUser);
-  }
-}
-
-// Component lists usersnames and balances
-class UserList extends Component {
-  template() {
-    return (`
-        <div class="title"><p class=username>User</p><p class="balance">Balance</p></div>
-        <ul class="userlist">
-        ${this.users.value.map(user => `<li>${new UserListItem({ user, history: this.history, users: this.users, parentComponent: this }).target()}</li>`).join("")}
-        </ul>
-    `)
-  }
-}
-
-// Component userlist-item
-class UserListItem extends Component {
-  removeUser() {
-    this.users.setValue(this.users.value.filter(user => user.uid !== this.user.uid));
-    this.history.setValue([...this.history.value, { id: this.generateID(), type: "removeuser", time: this.getTime(), uid: this.user.uid, username: this.user.name }])
-  }
-  template() {
-    return (`
-      <p class=username>${this.user.name}</p>
-      <p class="balance">${this.user.balance} ₺</p>
-      <button class="removeuser-button"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 0 24 24" width="26px" fill="#4b7bec"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg></button>
-    `)
-  }
-  addListeners() {
-    this.targetElement.querySelector(".removeuser-button").addEventListener("click", this.removeUser);
-  }
-}
-
-// Component manages transfers --- subscribes to users state
+// Component manages transfers
 class NewTransfer extends Component {
   transfer(e) {
     e.preventDefault();
@@ -141,7 +61,7 @@ class NewTransfer extends Component {
           <button class="send-button"> Send </button>
         </div>
       </form >
-  `)
+    `)
   }
 
   addListeners() {
@@ -175,7 +95,7 @@ class HistoryLogsHead extends Component {
     return (`
       <div class="title"> History </div>
       <div class="filters">
-        <div class="label"> Filter Transfers: </div>
+        <div class="label"> Filter By: </div>
         <input class="fromfilter-input" placeholder="From" pattern="([a-zA-Z]+[a-zA-Z ]+)" maxlength="20"/>
         <input class="tofilter-input" placeholder="To" pattern="([a-zA-Z]+[a-zA-Z ]+)" maxlength="20"/>
       </div>
@@ -272,9 +192,3 @@ class Log extends Component {
     if (this.log.type === "transfer" && !this.log.reverted) this.targetElement.querySelector(".undo-button").addEventListener("click", this.undoTransfer);
   }
 }
-
-// Initialize App
-const idCount = new idCounter();
-const root = document.querySelector("#root");
-const app = new App({ targetElement: root });
-app.render();
---
+++ src/usersPanel.js
@@ -0,0 +1,73 @@
+// Users-Panel
+class UsersPanel extends Component {
+  template() {
+    return (`
+    ${new NewUser({ users: this.users, history: this.history, parentComponent: this }).target()}
+    ${new UserList({ users: this.users, history: this.history, subscriptions: [this.users], parentComponent: this }).target()}
+    `)
+  }
+}
+
+// Component adds new users to the list
+class NewUser extends Component {
+  addUser(e) {
+    e.preventDefault();
+    // Get input elements
+    const usernameInput = this.targetElement.querySelector(".username-input");
+    const balanceInput = this.targetElement.querySelector(".balance-input");
+    // Add new user to users state
+    let uid = this.generateID()
+    this.users.setValue([...this.users.value, { name: usernameInput.value, uid: uid, balance: Number(balanceInput.value) }])
+    this.history.setValue([...this.history.value, { id: this.generateID(), type: "adduser", time: this.getTime(), username: usernameInput.value, uid: uid, balance: balanceInput.value }])
+    // Clear input fields
+    usernameInput.value = "";
+    balanceInput.value = "";
+  }
+  template() {
+    return (`
+      <div class="title"> Add new user </div>
+      <form class="adduser-form">
+        <div class="inputs-container">
+          <input class="username-input" placeholder="Name" required pattern="([a-zA-Z]+[a-zA-Z ]+)" maxlength="20"/>
+          <input class="balance-input" type="number" placeholder="Balance" required min="0" max="999999999"/>
+        </div>
+        <div class="button-container">
+          <button class="adduser-button"> Add </button>
+        </div>
+      </form>
+    `);
+  }
+  addListeners() {
+    this.targetElement.querySelector(".adduser-form").addEventListener("submit", this.addUser);
+  }
+}
+
+// Component lists usersnames and balances
+class UserList extends Component {
+  template() {
+    return (`
+        <div class="title"><p class=username>User</p><p class="balance">Balance</p></div>
+        <ul class="userlist">
+        ${this.users.value.map(user => `<li>${new UserListItem({ user, history: this.history, users: this.users, parentComponent: this }).target()}</li>`).join("")}
+        </ul>
+    `)
+  }
+}
+
+// Component userlist-item
+class UserListItem extends Component {
+  removeUser() {
+    this.users.setValue(this.users.value.filter(user => user.uid !== this.user.uid));
+    this.history.setValue([...this.history.value, { id: this.generateID(), type: "removeuser", time: this.getTime(), uid: this.user.uid, username: this.user.name }])
+  }
+  template() {
+    return (`
+      <p class=username>${this.user.name}</p>
+      <p class="balance">${this.user.balance} ₺</p>
+      <button class="removeuser-button"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 0 24 24" width="26px" fill="#4b7bec"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg></button>
+    `)
+  }
+  addListeners() {
+    this.targetElement.querySelector(".removeuser-button").addEventListener("click", this.removeUser);
+  }
+}
