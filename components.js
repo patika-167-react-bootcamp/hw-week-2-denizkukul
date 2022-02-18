402502df15668287da7d9d1a546814e87b9a316f
Deniz Kukul <deniz_kukul@hotmail.com>
2022-02-18 06:08:51 +0300

    Initial Commit

--- components.js
+++ components.js
@@ -1,104 +1,3 @@
-class State {
-  constructor(value) {
-    this.value = value;
-    this.subscribers = [];
-  }
-  addSubscriber(component) {
-    this.subscribers.push(component);
-  }
-  removeSubscriber(component) {
-    this.subscribers = this.subscribers.filter(this.subscriber !== component);
-  }
-  setValue(newValue) {
-    this.value = newValue;
-    this.subscribers.forEach(component => component.render());
-  }
-}
-
-class idGenerator {
-  constructor() {
-    this.value = 1;
-  }
-  getID() {
-    let id = this.value
-    this.value += 1;
-    return id;
-  }
-}
-
-// Parent class for all components
-class Component {
-  constructor(args) {
-    // Get all arguments passed to constructor
-    for (let arg in args) {
-      this[arg] = args[arg];
-    }
-    this.targetElementID = `component-${idCounter.getID()}`
-    this.childComponents = [];
-    this.parentComponent && this.parentComponent.childComponents.push(this);
-
-    this.subscribe();
-    this.bindAll();
-  }
-
-  // Get all defined class methods and bind all to this object
-  // This is needed for "this" inside event handler functions to refer to component object instead of the target element;
-  bindAll() {
-    const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
-    methods.forEach((method) => {
-      if (method !== 'constructor') {
-        this[method] = this[method].bind(this);
-      }
-    })
-  }
-
-  // Method to get date and time
-  getTime() {
-    let today = new Date();
-    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return date + ' ' + time;
-  }
-
-  // Add this component to subscribers of related states
-  subscribe() {
-    this.subscriptions && this.subscriptions.forEach(state => state.addSubscriber(this));
-  }
-
-  // Search parent component to get target element with id
-  getTargetElement() {
-    this.targetElement = document.querySelector(`#${this.targetElementID}`);
-    // If component target no longer exists, remove it from state subscribers
-    if (!this.targetElement) this.subscriptions.forEach(removeSubscriber(this));
-  }
-
-  // Trigger render of all child components
-  renderChildComponents() {
-    this.childComponents.forEach(component => component.render());
-  }
-
-  // Return target element for this component;
-  target() {
-    return (`
-      <div id=${this.targetElementID} class="${this.constructor.name.toLowerCase()}-component"></div>
-    `)
-  }
-
-  // Render this components template in target element
-  render() {
-    // If this is initial render get target element
-    !this.targetElement && this.getTargetElement();
-    // Set innerHTML of this components target element
-    this.targetElement.innerHTML = this.template();
-    // Trigger render of all child components
-    this.childComponents && this.renderChildComponents();
-    // If this component uses event listeners, add event listeners
-    this.addListeners && this.addListeners();
-  }
-  // Components must be created as a new class extending Component class with template method
-  // Event listeners must be added in addListeners method
-}
-
 // Main component renders body element
 class App extends Component {
   constructor(args) {
@@ -241,11 +140,6 @@ class NewTransfer extends Component {
   }
 }
 
-// Component returns select element with user options
-class UserSelect extends Component {
-
-}
-
 // Component lists logs of transfers
 class HistoryLogs extends Component {
   template() {
@@ -277,7 +171,7 @@ class Log extends Component {
 
 
 // Initialize App
-const idCounter = new idGenerator();
+const idCount = new idCounter();
 const root = document.querySelector("#root");
 const app = new App({ targetElement: root });
 app.render();
